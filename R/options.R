8c272026286d25ad177a7c251fafeeb4bab45511
Alex Chubaty <achubaty@for-cast.ca>
2022-09-30 10:37:16 -0600

    improve messaging in setProjectOptions()

--- R/options.R
+++ R/options.R
@@ -15,11 +15,12 @@ setProjectOptions <- function(config) {
     .needPkg("raster", "stop")
   }
 
+  message("Setting options specified in config$options...")
   opts <- options(config$options)
 
   if (requireNamespace("raster", quietly = TRUE)) {
     httr::set_config(httr::config(http_version = 0)) ## TODO: is this still needed??
-    message("Setting httr option `http_version = 0`, which is needed to download",
+    message("Additonally setting httr option `http_version = 0`, which is needed to download",
             " from from certain websites.")
   } else {
     .needPkg("httr", "stop")
