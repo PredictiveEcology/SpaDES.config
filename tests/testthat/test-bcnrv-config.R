c43e9e0b4967c3a57fe45c98830517b5f4b7329e
Alex Chubaty <achubaty@for-cast.ca>
2024-04-03 22:03:39 -0600

    update BC NRV tests for scfm runs  using box

--- tests/testthat/test-bcnrv-config.R
+++ tests/testthat/test-bcnrv-config.R
@@ -1,28 +1,43 @@
-test_that("BC NRV config + context setup is working", {
-  skip("Run manually with latest version of BC_HRV Project on test machine.")
+test_that("BC NRV [SCFM] config + context setup is working", {
+  skip("Run manually with latest version of BC_HRV project on test machine.")
 
   ## project: BC_HRV ------------------------------------------------------------------------------
   prjDir <- "~/GitHub/BC_HRV"
 
   pr_mods <- list("BC_HRV_preamble", "Biomass_borealDataPrep", "Biomass_core",
-                  "Biomass_regeneration", "Biomass_speciesData", "LandWeb_output",
-                  "scfmDiagnostics", "scfmDriver", "scfmEscape", "scfmIgnition",
-                  "scfmLandcoverInit", "scfmRegime", "scfmSpread", "timeSinceFire")
+                  "Biomass_regeneration", "Biomass_speciesData", "Biomass_speciesParameters",
+                  "LandWeb_output", "timeSinceFire") |>
+    append(list("scfmDiagnostics", "scfmDriver", "scfmEscape", "scfmIgnition",
+                "scfmLandcoverInit", "scfmRegime", "scfmSpread"))
   names(pr_mods) <- pr_mods
   dv_mods <- pr_mods
   pp_mods <- list("BC_HRV_preamble", "Biomass_speciesData", "HSI_PineMarten",
-                  "LandWeb_summary", "NRV_summary", "scfmDiagnostics")
+                  "LandWeb_summary", "NRV_summary") |>
+    append(list("scfmDiagnostics"))
   names(pp_mods) <- pp_mods
 
   .studyAreaName <- c("Corkscrew", "Christenson Creek", "Downton", "Punky Moore")
-  config.bc <- suppressWarnings({
-    useConfig(projectName = "BC_NRV", projectPath = prjDir,
-              mode = "development", rep = 1L, studyAreaName = .studyAreaName)
-  })
+
+  boxDir <- file.path("tests", "testthat", "box")
+  boxMod <- file.path(prjDir, "box", "bcnrv.R")
+
+  if (!dir.exists(boxDir)) dir.create(boxDir)
+  if (!file.exists(boxMod)) file.symlink(boxMod, file.path(boxDir, "bcnrv.R"))
+
+  box::use(./box/bcnrv)
+
+  expWarn <- "Parameters specified for modules not found in `modules` and will be ignored"
+  suppressWarnings({
+    config.bc <- bcnrv$bcnrvConfig$new(
+      projectPath = prjDir,
+      fireModel = "scfm", nrvType = "hrv",
+      mode = "development", rep = 1L, studyAreaName = .studyAreaName
+    )$update()$validate()
+  }) ## can't use expect_warning with object assignment
 
   ## context
   expect_equal(config.bc$context[["runName"]],
-               "multiple_LUs_n04_d6b6441dd2d0e467_BECSUBZONE_res125_rep01", ignore_attr = TRUE)
+               "multiple_LUs_n04_d6b6441dd2d0e467_scfm_hrv_BECSUBZONE_res125_rep01", ignore_attr = TRUE)
 
   ## args
   expect_equal(config.bc$args[["delayStart"]], 0L)
@@ -46,30 +61,32 @@ test_that("BC NRV config + context setup is working", {
   expect_identical(config.bc$params$scfmDiagnostics$mode, "single")
 
   ## paths
-  expect_identical(fs::is_absolute_path(unlist(config.bc$paths)),
+  fs_is_abs_paths <- fs::is_absolute_path(unlist(config.bc$paths))
+  names(fs_is_abs_paths) <- names(unlist(config.bc$paths))
+  expect_identical(fs_is_abs_paths,
                    c(cachePath = FALSE, inputPath = FALSE, logPath = FALSE,
                      modulePath1 = FALSE, modulePath2 = FALSE, outputPath = FALSE,
                      projectPath = TRUE, scratchPath = TRUE, tilePath = FALSE))
   expect_identical(
-    .getRelativePath(config.bc$paths[["outputPath"]], prjDir),
-    file.path("outputs", "multiple_LUs_n04_d6b6441dd2d0e467_BECSUBZONE_res125", "rep01")
+    SpaDES.config:::.getRelativePath(config.bc$paths[["outputPath"]], prjDir),
+    file.path("outputs", "multiple_LUs_n04_d6b6441dd2d0e467_scfm_hrv_BECSUBZONE_res125", "rep01")
   )
   expect_identical(
-    .getRelativePath(config.bc$paths[["logPath"]], prjDir),
-    file.path(.getRelativePath(config.bc$paths[["outputPath"]], prjDir), "log")
+    SpaDES.config:::.getRelativePath(config.bc$paths[["logPath"]], prjDir),
+    file.path(SpaDES.config:::.getRelativePath(config.bc$paths[["outputPath"]], prjDir), "log")
   )
   expect_identical(
-    .getRelativePath(config.bc$paths[["tilePath"]], prjDir),
-    file.path(.getRelativePath(config.bc$paths[["outputPath"]], prjDir), "tiles")
+    SpaDES.config:::.getRelativePath(config.bc$paths[["tilePath"]], prjDir),
+    file.path(SpaDES.config:::.getRelativePath(config.bc$paths[["outputPath"]], prjDir), "tiles")
   )
 
   ## single study area name using NRD/NRR ----------------------------------------------------------
   config.bc$context$studyAreaName <- "NRR_Cariboo"
-  config.bc$update()$validate()
+  expect_warning(config.bc$update()$validate(), expWarn)
 
   ## context
   expect_equal(config.bc$context[["runName"]],
-               "NRR_Cariboo_BECSUBZONE_res125_rep01", ignore_attr = TRUE)
+               "NRR_Cariboo_scfm_hrv_BECSUBZONE_res125_rep01", ignore_attr = TRUE)
 
   ## args
   ##
@@ -84,22 +101,24 @@ test_that("BC NRV config + context setup is working", {
   #expect_identical(config.bc$params$BC_HRV_preamble$landscapeUnits, "NRR_Cariboo") ## TODO:
 
   ## paths
-  expect_identical(fs::is_absolute_path(unlist(config.bc$paths)),
+  fs_is_abs_paths <- fs::is_absolute_path(unlist(config.bc$paths))
+  names(fs_is_abs_paths) <- names(unlist(config.bc$paths))
+  expect_identical(fs_is_abs_paths,
                    c(cachePath = FALSE, inputPath = FALSE, logPath = FALSE,
                      modulePath1 = FALSE, modulePath2 = FALSE, outputPath = FALSE,
                      projectPath = TRUE, scratchPath = TRUE, tilePath = FALSE))
   expect_identical(
-    .getRelativePath(config.bc$paths[["outputPath"]], prjDir),
-    file.path("outputs", "NRR_Cariboo_BECSUBZONE_res125", "rep01")
+    SpaDES.config:::.getRelativePath(config.bc$paths[["outputPath"]], prjDir),
+    file.path("outputs", "NRR_Cariboo_scfm_hrv_BECSUBZONE_res125", "rep01")
   )
 
   ## fire regime polygon types ---------------------------------------------------------------------
   config.bc$context$frpType <- "ECODISTRICT"
-  config.bc$update()$validate()
+  expect_warning(config.bc$update()$validate(), expWarn)
 
   ## context
   expect_equal(config.bc$context[["runName"]],
-               "multiple_LUs_n04_d6b6441dd2d0e467_ECODISTRICT_res125_rep01", ignore_attr = TRUE)
+               "NRR_Cariboo_scfm_hrv_ECODISTRICT_res125_rep01", ignore_attr = TRUE)
 
   ## args
   ##
@@ -114,30 +133,32 @@ test_that("BC NRV config + context setup is working", {
   expect_identical(config.bc$params$BC_HRV_preamble$fireRegimePolysType, "ECODISTRICT")
 
   ## paths
-  expect_identical(fs::is_absolute_path(unlist(config.bc$paths)),
+  fs_is_abs_paths <- fs::is_absolute_path(unlist(config.bc$paths))
+  names(fs_is_abs_paths) <- names(unlist(config.bc$paths))
+  expect_identical(fs_is_abs_paths,
                    c(cachePath = FALSE, inputPath = FALSE, logPath = FALSE,
                      modulePath1 = FALSE, modulePath2 = FALSE, outputPath = FALSE,
                      projectPath = TRUE, scratchPath = TRUE, tilePath = FALSE))
   expect_identical(
-    .getRelativePath(config.bc$paths[["outputPath"]], prjDir),
-    file.path("outputs", "multiple_LUs_n04_d6b6441dd2d0e467_ECODISTRICT_res125", "rep01")
+    SpaDES.config:::.getRelativePath(config.bc$paths[["outputPath"]], prjDir),
+    file.path("outputs", "NRR_Cariboo_scfm_hrv_ECODISTRICT_res125", "rep01")
   )
   expect_identical(
-    .getRelativePath(config.bc$paths[["logPath"]], prjDir),
-    file.path(.getRelativePath(config.bc$paths[["outputPath"]], prjDir), "log")
+    SpaDES.config:::.getRelativePath(config.bc$paths[["logPath"]], prjDir),
+    file.path(SpaDES.config:::.getRelativePath(config.bc$paths[["outputPath"]], prjDir), "log")
   )
   expect_identical(
-    .getRelativePath(config.bc$paths[["tilePath"]], prjDir),
-    file.path(.getRelativePath(config.bc$paths[["outputPath"]], prjDir), "tiles")
+    SpaDES.config:::.getRelativePath(config.bc$paths[["tilePath"]], prjDir),
+    file.path(SpaDES.config:::.getRelativePath(config.bc$paths[["outputPath"]], prjDir), "tiles")
   )
 
   ## mode postprocess ------------------------------------------------------------------------------
   config.bc$context$mode <- "postprocess"
-  config.bc$update()$validate()
+  expect_warning(config.bc$update()$validate(), expWarn)
 
   ## context
   expect_equal(config.bc$context[["runName"]],
-               "multiple_LUs_n04_d6b6441dd2d0e467_ECODISTRICT_res125", ignore_attr = TRUE)
+               "NRR_Cariboo_scfm_hrv_ECODISTRICT_res125", ignore_attr = TRUE)
 
   ## args
   expect_equal(config.bc$args[["delayStart"]], 0L)
@@ -153,21 +174,23 @@ test_that("BC NRV config + context setup is working", {
   expect_identical(config.bc$params$scfmDiagnostics$mode, "multi")
 
   ## paths
-  expect_identical(fs::is_absolute_path(unlist(config.bc$paths)),
+  fs_is_abs_paths <- fs::is_absolute_path(unlist(config.bc$paths))
+  names(fs_is_abs_paths) <- names(unlist(config.bc$paths))
+  expect_identical(fs_is_abs_paths,
                    c(cachePath = FALSE, inputPath = FALSE, logPath = FALSE,
                      modulePath1 = FALSE, modulePath2 = FALSE, outputPath = FALSE,
                      projectPath = TRUE, scratchPath = TRUE, tilePath = FALSE))
   expect_identical(
-    .getRelativePath(config.bc$paths[["outputPath"]], prjDir),
-    file.path("outputs", "multiple_LUs_n04_d6b6441dd2d0e467_ECODISTRICT_res125")
+    SpaDES.config:::.getRelativePath(config.bc$paths[["outputPath"]], prjDir),
+    file.path("outputs", "NRR_Cariboo_scfm_hrv_ECODISTRICT_res125")
   )
   expect_identical(
-    .getRelativePath(config.bc$paths[["logPath"]], prjDir),
-    file.path(.getRelativePath(config.bc$paths[["outputPath"]], prjDir), "log")
+    SpaDES.config:::.getRelativePath(config.bc$paths[["logPath"]], prjDir),
+    file.path(SpaDES.config:::.getRelativePath(config.bc$paths[["outputPath"]], prjDir), "log")
   )
   expect_identical(
-    .getRelativePath(config.bc$paths[["tilePath"]], prjDir),
-    file.path(.getRelativePath(config.bc$paths[["outputPath"]], prjDir), "tiles")
+    SpaDES.config:::.getRelativePath(config.bc$paths[["tilePath"]], prjDir),
+    file.path(SpaDES.config:::.getRelativePath(config.bc$paths[["outputPath"]], prjDir), "tiles")
   )
 
   rm(config.bc)
